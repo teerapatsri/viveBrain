1f2c28b68ca3cf38ff258856628be0dae6ee80f6
Teerapatsri <tearapatsri@gmail.com>
2016-10-11 11:17:41 +0700

    vive first look

---
+++ Vive/Assets/SteamVR/Scripts/Interactable.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable : MonoBehaviour {
+	public Rigidbody rigidbody;
+
+	private bool currentlyInteracting;
+	private WandController attachedWand;
+	private Transform interactionPoint;
+	private Vector3 posDelta, axis;
+	private Quaternion rotationDelta;
+	private float angle;
+	private float rotationFactor = 400f;
+	private float velocityFactor = 20000f;
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody> ();
+		interactionPoint = new GameObject ().transform;
+		velocityFactor /= rigidbody.mass;
+		rotationFactor /= rigidbody.mass;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (attachedWand != null && currentlyInteracting) {
+			posDelta = attachedWand.transform.position - interactionPoint.position;
+			this.rigidbody.velocity = posDelta * velocityFactor * Time.fixedDeltaTime;
+
+			rotationDelta = attachedWand.transform.rotation * Quaternion.Inverse (interactionPoint.rotation);
+			rotationDelta.ToAngleAxis (out angle, out axis);
+
+			if (angle > 180) {
+				angle -= 360;
+			}
+
+			this.rigidbody.angularVelocity = (Time.fixedDeltaTime * angle * axis) * rotationFactor;
+		}
+	}
+
+	public void BeginInteraction(WandController wand){
+		attachedWand = wand;
+		interactionPoint.position = wand.transform.position;
+		interactionPoint.rotation = wand.transform.rotation;
+		interactionPoint.SetParent (transform, true);
+
+		currentlyInteracting = true;
+	}
+	public void EndInteraction(WandController wand){
+		if (wand == attachedWand) {
+			attachedWand = null;
+			currentlyInteracting = false;
+		}
+	}
+
+	public bool IsInteracting(){
+		return currentlyInteracting;
+	}
+}
---
+++ Vive/Assets/SteamVR/Scripts/WandController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WandController : MonoBehaviour {
+	private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+	private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
+
+	private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input ((int)trackedObj.index); } }
+	private SteamVR_TrackedObject trackedObj;
+
+	HashSet<Interactable> objectHoveringOver = new HashSet<Interactable>();
+	private Interactable closestItem;
+	private Interactable interactingItem;
+	// Use this for initialization
+	void Start () {
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(controller==null){
+			Debug.Log ("Controller not initialized");
+			return;
+		}
+
+		if (controller.GetPressDown (gripButton)) {
+			float minDistance = float.MaxValue;
+			float distance;
+			foreach (Interactable item in objectHoveringOver) {
+				distance = (item.transform.position - transform.position).sqrMagnitude;
+				if (distance < minDistance) {
+					minDistance = distance;
+					closestItem = item;
+				}
+			}
+
+			interactingItem = closestItem;
+
+//			if (interactingItem != null) {
+//				if (interactingItem.IsInteracting ()) {
+//					interactingItem.EndInteraction (this);
+//				}
+//
+//				interactingItem.BeginInteraction (this);
+//			}
+		}
+		if (controller.GetPressUp (gripButton)&& interactingItem!=null) {
+			interactingItem.EndInteraction (this);
+		}
+	}
+
+	private void OnTriggerEnter(Collider other){
+		Interactable collidedItem = GetComponent<Collider>().GetComponent<Interactable>();
+		if (collidedItem != null) {
+			objectHoveringOver.Add (collidedItem);
+		}
+	}
+	private void OnTriggerExit(Collider other){
+		Interactable collidedItem = GetComponent<Collider>().GetComponent<Interactable> ();
+		if (collidedItem != null) {
+			objectHoveringOver.Remove (collidedItem);
+		} 
+	}
+}
