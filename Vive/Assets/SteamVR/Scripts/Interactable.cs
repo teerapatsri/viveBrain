0f9561b66ee1401be645f961756cb49a7e5fbaf1
tee <tearapatsri@gmail.com>
2016-10-18 03:02:57 -0700

    ROTATE SUCCESSFUL!!

--- Vive/Assets/SteamVR/Scripts/Interactable.cs
+++ Vive/Assets/SteamVR/Scripts/Interactable.cs
@@ -1,83 +1,93 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Interactable : MonoBehaviour {
-	public new Rigidbody rigidbody;
+public class Interactable : MonoBehaviour
+{
+    public new Rigidbody rigidbody;
     public WandController wand1;
     public WandController wand2;
 
     private bool currentlyGrabbing;
     private bool currentlyTrigger;
     private WandController attachedWand;
-	private Transform interactionPoint;
-	private Vector3 posDeltaG, posDeltaT, axis;
-	private Quaternion rotationDelta;
+    private Transform interactionPoint;
+    private Vector3 posDeltaG, posDeltaT, axis;
+    private Quaternion preRotation, rotationDelta;
     private float startDistance;
     private float distance;
-	private float angle;
+    private float angle;
     private float change;
-	private float rotationFactor = 400f;
-	private float velocityFactor = 20000f;
-    private Vector3 defaultScale;
+    private float rotationFactor = 40000f;
+    private float velocityFactor = 20000f;
+    private Vector3 defaultScale, preScale;
     // Use this for initialization
-    void Start () {
-		rigidbody = GetComponent<Rigidbody> ();
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
         defaultScale = transform.localScale;
-        interactionPoint = new GameObject ().transform;
-		velocityFactor /= rigidbody.mass;
-		rotationFactor /= rigidbody.mass;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //case grab
-		if (attachedWand != null && currentlyGrabbing) {
-			posDeltaG = attachedWand.transform.position - interactionPoint.position;
-			this.rigidbody.velocity = posDeltaG * velocityFactor * Time.fixedDeltaTime;
+        preScale = defaultScale;
+        preRotation = transform.rotation;
+        interactionPoint = new GameObject().transform;
+        velocityFactor /= rigidbody.mass; //mass makes object go slower
+        rotationFactor /= rigidbody.mass;  // and harder to rotate?
+    }
 
-			//rotationDelta = attachedWand.transform.rotation * Quaternion.Inverse (interactionPoint.rotation);
-			//rotationDelta.ToAngleAxis (out angle, out axis);
-
-            //this.rigidbody.angularVelocity = (Time.fixedDeltaTime * angle * axis) * rotationFactor;
+    //rotation example
+    // Update is called once per frame
+    void Update()
+    {
+        //case grab = move object around
+        if (attachedWand != null && currentlyGrabbing)
+        {
+            posDeltaG = attachedWand.transform.position - interactionPoint.position; //position changed
+            rigidbody.velocity = posDeltaG * velocityFactor * Time.deltaTime;
         }
-        //case 1 trigger
+        //case single trigger = rotate object
         if (attachedWand != null && currentlyTrigger && ((wand1.trigger && wand2.trigger) == false)) //if both triggered go to case 3
         {
-            rotationDelta = Quaternion.Euler(new Vector3(transform.position.z - attachedWand.transform.position.z, transform.position.x - attachedWand.transform.position.x, transform.position.y - attachedWand.transform.position.y));
+            rotationDelta = Quaternion.LookRotation(attachedWand.transform.position - transform.position) * Quaternion.Inverse(preRotation);
+            //rotationDelta *= rotationDelta;
+            rotationDelta.x = 0;
+            rotationDelta.z = 0;
             rotationDelta.ToAngleAxis(out angle, out axis);
-            this.rigidbody.angularVelocity = (Time.fixedDeltaTime * angle * axis) * rotationFactor;
+            rigidbody.angularVelocity = (Time.deltaTime * angle * axis) * rotationFactor;
+            //transform.rotation = Quaternion.Slerp(transform.rotation, transform.rotation * rotationDelta, rotationFactor * Time.deltaTime);
+            preRotation = Quaternion.LookRotation(attachedWand.transform.position - transform.position);
         }
-        //case double trigger
-        if(wand1 != null && wand2 != null && wand1.trigger && wand2.trigger)
+        //case double trigger = ZOOM (Scale) object
+        if (wand1 != null && wand2 != null && wand1.trigger && wand2.trigger)
         {
-           distance = Vector3.Distance(wand1.transform.position, wand2.transform.position);
-           change = distance - startDistance;
-           transform.localScale = new Vector3(defaultScale.x * (1+change), defaultScale.y * (1+change), defaultScale.z * (1+change));
-           //compare with startDistance then change 
+            distance = Vector3.Distance(wand1.transform.position, wand2.transform.position); //distance between wands
+            change = distance - startDistance;   //difference of default and current distance
+            transform.localScale = new Vector3(Mathf.Clamp(preScale.x * (1 + change), defaultScale.x * 0.1f, defaultScale.x * 3f),
+                                                 Mathf.Clamp(preScale.y * (1 + change), defaultScale.y * 0.1f, defaultScale.y * 3f),
+                                                 Mathf.Clamp(preScale.z * (1 + change), defaultScale.z * 0.1f, defaultScale.z * 3f));
+
+            //compare with startDistance then change 
         }
     }
 
-	public void BeginGrab(WandController wand){
-		attachedWand = wand;
-		interactionPoint.position = wand.transform.position;
-		interactionPoint.rotation = wand.transform.rotation;
-		interactionPoint.SetParent (transform, true);
-
-		currentlyGrabbing = true;
-	}
-	public void EndGrab(WandController wand){
-		if (wand == attachedWand) {
-			attachedWand = null;
-			currentlyGrabbing = false;
-		}
-	}
-    public void BeginTrigger(WandController wand)
+    public void BeginGrab(WandController wand)
     {
         attachedWand = wand;
         interactionPoint.position = wand.transform.position;
         interactionPoint.rotation = wand.transform.rotation;
         interactionPoint.SetParent(transform, true);
 
+        currentlyGrabbing = true;
+    }
+    public void EndGrab(WandController wand)
+    {
+        if (wand == attachedWand)
+        {
+            attachedWand = null;
+            currentlyGrabbing = false;
+        }
+    }
+    public void BeginTrigger(WandController wand)
+    {
+        attachedWand = wand;
+        preRotation = Quaternion.LookRotation(attachedWand.transform.position - transform.position);
         currentlyTrigger = true;
     }
     public void EndTrigger(WandController wand)
@@ -91,17 +101,19 @@ public class Interactable : MonoBehaviour {
     public void BeginZoom()
     {
         startDistance = Vector3.Distance(wand1.transform.position, wand2.transform.position);
-        currentlyTrigger = false;
+        currentlyTrigger = false; //to ensure getting out of rotating mode
     }
     public void EndZoom()
     {
-        defaultScale = transform.localScale;
+        preScale = transform.localScale; //redefine defaultScale
         currentlyTrigger = true;
     }
-    public bool IsGrabbing(){
-		return currentlyGrabbing;
-	}
-    public bool IsTriggering(){
+    public bool IsGrabbing()
+    {
+        return currentlyGrabbing;
+    }
+    public bool IsTriggering()
+    {
         return currentlyTrigger;
     }
 }
--- Vive/Assets/SteamVR/Scripts/Rotator.cs
+++ Vive/Assets/SteamVR/Scripts/Rotator.cs
@@ -2,8 +2,6 @@
 using System.Collections;
 
 public class Rotator : MonoBehaviour {
-	public WandController wandControllerLeft;
-	public WandController wandControllerRight;
 	// Update is called once per frame
 	void Update () {
 			transform.Rotate (new Vector3 (0,15,0)* Time.deltaTime);
