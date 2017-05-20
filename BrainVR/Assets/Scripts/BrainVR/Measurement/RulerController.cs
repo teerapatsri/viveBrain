7cabff083a4a3692522acfa9912d6feab9df649d
Thanat Jatuphattharachat <thanat.jat@gmail.com>
2017-05-20 18:56:56 +0700

    Create Ruler With Cylinder

--- BrainVR/Assets/Scripts/BrainVR/Measurement/RulerController.cs
+++ BrainVR/Assets/Scripts/BrainVR/Measurement/RulerController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class RulerController : MonoBehaviour {
 	
+	private GameObject lengthIndicatorWrapper;
 	private GameObject lengthIndicator;
-	private LineRenderer lengthIndicatorRenderer;
+	
+	private Renderer lengthIndicatorRenderer;
 	private float lengthIndicatorDiameter = 0.02F;
 	private int nPin = 0;
 	private bool isPinSecondPoint = false;
@@ -16,38 +18,38 @@ public class RulerController : MonoBehaviour {
     public Transform markerObject;
 	public Material rulerShader;
 
+	// Cylinder Ruler 
+
 	void Start () {
 		InitializeLengthIndicator();
 	}
-	
+
 	void Update () {
 		
 	}
 
-	void InitializeLengthIndicator() {
-		lengthIndicator = new GameObject();
-		lengthIndicator.transform.position = Vector3.zero;
-		lengthIndicator.AddComponent<LineRenderer>();
-
-		lengthIndicatorRenderer = lengthIndicator.GetComponent<LineRenderer>();
-
-		lengthIndicatorRenderer.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
-		// lengthIndicatorRenderer.material = rulerShader;
-
-		lengthIndicatorRenderer.startColor = Color.green;
-		lengthIndicatorRenderer.endColor = Color.green;
-
-		lengthIndicatorRenderer.startWidth = lengthIndicatorDiameter;
-		lengthIndicatorRenderer.endWidth = lengthIndicatorDiameter;
-
-		lengthIndicatorRenderer.SetPosition(0, Vector3.zero);
-		lengthIndicatorRenderer.SetPosition(1, Vector3.zero);
+	void DrawLengthIndicator () {
+		lengthIndicatorWrapper.transform.position = (secondPoint - firstPoint)/2 + firstPoint;
+		lengthIndicatorWrapper.transform.LookAt(secondPoint);
+		
+		// Scale
+		var v3T = lengthIndicatorWrapper.transform.localScale;
+		v3T.y = (secondPoint-firstPoint).magnitude/2;
+		v3T.z = 0.01f;
+		v3T.x = 0.01f;
+		lengthIndicatorWrapper.transform.localScale = v3T;
+		
+		// Rotate
+		lengthIndicatorWrapper.transform.rotation = Quaternion.FromToRotation(Vector3.up, secondPoint-firstPoint);
 	}
 
-	void DrawLine()
-	{
-		lengthIndicatorRenderer.SetPosition(0, firstPoint);
-		lengthIndicatorRenderer.SetPosition(1, secondPoint);	
+	void InitializeLengthIndicator() {
+		lengthIndicatorWrapper = new GameObject();
+		lengthIndicator = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+		// lengthIndicator.transform.position = Vector3.zero;
+		lengthIndicatorRenderer = lengthIndicator.GetComponent<Renderer>();
+		lengthIndicatorRenderer.material.color = Color.green;
+		lengthIndicator.transform.parent = lengthIndicatorWrapper.transform;
 	}
 
 	public void PinPoint(Vector3 pinPoint)
@@ -67,7 +69,8 @@ public class RulerController : MonoBehaviour {
 			isPinSecondPoint = true;
 			secondPoint = pinPoint;
 		}
-		DrawLine();
+		// DrawLine();
+		DrawLengthIndicator();
 		nPin = (nPin + 1) % 2;
 	}
 
@@ -77,7 +80,7 @@ public class RulerController : MonoBehaviour {
 		{
 			secondPoint = currentPoint;
 		}
-		DrawLine();
+		DrawLengthIndicator();
 	}
 
 }
